4e3450e196ee562aff419e5d7aa3e22e085d8ddc
Marcin Polak <mapoart@gmail.com>
2019-11-28 17:42:09 +0100

    Changed Hello World Message to be consistent with other languages.

--- templates/helloWorld.lua
+++ templates/helloWorld.lua
@@ -1,13 +1,14 @@
 -- Nexss PROGRAMMER 2.0.0 - Lua
--- Default template for JSON Data
+-- Lua Hello World
 json = require "json"
 -- STDIN
 local NexssStdin = io.read()
 local parsedJson = json.decode(NexssStdin)
 
 -- Modify Data
-parsedJson['luaOutput'] = "Hello from Lua ! " .. _VERSION
+parsedJson['HelloFromLua'] = _VERSION
 -- parsedJson['x'] = parsedJson['x'] + 1234
+-- " ! " .. _VERSION
 
 local NexssStdout = json.encode(parsedJson)
 -- STDOUT
