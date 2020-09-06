e39494a8a519cbc922aa6b804629f34bb14cf123
Marcin Polak <mapoart@gmail.com>
2020-09-06 15:21:02 +0200

    all templates configured with Linux

--- templates/helloWorld.lua.js
+++ templates/helloWorld.lua.js
@@ -1,8 +1,19 @@
+function getInstaller() {
+  switch (process.platform) {
+    case "win32":
+      return `Powershell -Command "if (Test-Path "src\\json.lua" ) { cd src ; mv json.lua ../json.lua ; cd ..}"`;
+    case "linux":
+    case "darwin":
+      return 'if test -f "src/json.lua"; then cd src ; mv json.lua ../json.lua ; cd .. ; fi;';
+    default:
+      console.error(`${process.platform} is not implemented.`);
+      break;
+  }
+}
+
 const config = {
   files: ["json.lua"],
-  commands: [
-    `Powershell -Command "if (Test-Path "src\\json.lua" ) { cd src ; mv json.lua ../json.lua ; cd ..}"`,
-  ],
+  commands: [getInstaller()],
   repos: ["https://github.com/rxi/json.lua"],
 };
 
