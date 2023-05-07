f8d1a480adedccd03d9a652014682253e0ade7c8
Marcin Polak <mapoart@gmail.com>
2023-05-07 16:05:58 +0200

    Support for Android platforms

---
+++ lua.android.nexss.config.js
@@ -0,0 +1,14 @@
+let languageConfig = Object.assign({}, require("./lua.win32.nexss.config"));
+// let sudo = process.sudo;
+
+languageConfig.compilers = {
+  lua53: {
+    install: `pkg install -y lua53`,
+    // switch: "scoop reset lua", // If not exists install will be replaced by reset command
+    command: "lua5.3",
+    args: "<file>",
+    templates: `templates53`,
+  },
+};
+
+module.exports = languageConfig;
